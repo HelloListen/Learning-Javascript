65f672b267f1548b3de51c469a76e504ad789022
HelloListen <listenzhangbin@163.com>
2015-05-20 23:39:11 +0800

    update

--- listenzhangbin.com/scripts/listen.js
+++ listenzhangbin.com/scripts/listen.js
@@ -1,11 +1,20 @@
+
+$(function(){
+    $("#container").fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90','#aee2d9'],
+        navigation:true
+    });
+})
+
 $(function(){
     var left=($(window).width()-$("div.photo").width())/2;
     var top=($(window).height()-$("div.photo").height()-100)/2;
+    $("div.photo").css({"left":left+"px","top":top+100+"px"});
     $("div.photo").fadeTo(1500,1,function(){
-        $("div.photo").animate({"top":top+"px"},1000)
+        $("div.photo").animate({"top":top-50+"px"},1000)
     })
     $("div.title").fadeTo(1500,1,function(){
-        $("div.title").animate({"top":(top*3)+"px"},1000)
+        $("div.title").animate({"top":(top*2.8)+"px"},1000)
     })
-
 })
+
