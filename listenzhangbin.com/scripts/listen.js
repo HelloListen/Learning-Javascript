fba0a84f23e6f48dd392cf1bc8e5332fbbbe37bb
HelloListen <listenzhangbin@163.com>
2015-05-21 22:55:23 +0800

    resume.html finish

--- listenzhangbin.com/scripts/listen.js
+++ listenzhangbin.com/scripts/listen.js
@@ -1,20 +1,19 @@
 
 $(function(){
     $("#container").fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90','#aee2d9'],
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#B0C4DE','#aee2d9'],
         navigation:true
     });
 })
 
+function addSpotlight(){
+    $("div#navbar-collapse-main ul li").each(function(){
+        $(this).click(function(){
+            $("div#navbar-collapse-main ul li").removeClass("spotlight");
+            $(this).addClass("spotlight");
+        })
+    })
+}
 $(function(){
-    var left=($(window).width()-$("div.photo").width())/2;
-    var top=($(window).height()-$("div.photo").height()-100)/2;
-    $("div.photo").css({"left":left+"px","top":top+100+"px"});
-    $("div.photo").fadeTo(1500,1,function(){
-        $("div.photo").animate({"top":top-50+"px"},1000)
-    })
-    $("div.title").fadeTo(1500,1,function(){
-        $("div.title").animate({"top":(top*2.8)+"px"},1000)
-    })
+    addSpotlight();
 })
-
